d52db0bcd6eb87bbf9caabc051c9c0f67555eb8c
DidierSML <113539961+DidierSML@users.noreply.github.com>
2023-03-05 17:48:08 -0500

    2da prueba de Commit and Push desde IJ usando git

--- src/main/java/GitProof.java
+++ src/main/java/GitProof.java
@@ -1,6 +1,12 @@
 public class GitProof {
 
     public static void main(String[] args) {
-        System.out.println("Hola soy una prueba usando Git desde IJ");
+        System.out.println("Hola soy una prueba usando Git desde IJ");//1ra
+
+        String newMessage = "Probando el -PUSH- desde la (rama Local-Master) hacia la (rama Remota Master)";//2da
+        System.out.println(newMessage);
     }
+
+
+
 }
